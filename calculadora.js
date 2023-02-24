dcb58bdb7ee20d6092bfab86215115446087de58
Tekkiyayin <126032420+Tekkiyayin@users.noreply.github.com>
2023-02-23 23:52:19 -0500

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,26 @@
+
+console.log("suma")
+var numero1, numero2, total;
+numero1=parseInt(prompt("Ingrese el primer numero: "));
+numero2=parseInt(prompt("Ingrese el segundo numero: "));
+total=(numero1 + numero2 );
+alert("El resultado de la suma es:  "+ total);
+
+var numero1, numero2, total;
+numero1=parseInt(prompt("Ingrese el primer numero: "));
+numero2=parseInt(prompt("Ingrese el segundo numero: "));
+total=(numero1 - numero2 );
+alert("El resultado de la resta es:  "+ total);
+
+var numero1, numero2, total;
+numero1=parseInt(prompt("Ingrese el primer numero: "));
+numero2=parseInt(prompt("Ingrese el segundo numero: "));
+total=(numero1 * numero2 );
+alert("El resultado de la multilpicacion es:  "+ total);
+
+var numero1, numero2, total;
+numero1=parseInt(prompt("Ingrese el primer numero: "));
+numero2=parseInt(prompt("Ingrese el segundo numero: "));
+total=(numero1/numero2 );
+alert("El resultado de la division es:  "+ total);
+
